2e6bfbf6777aa940b5c20847ef041ad3593edaf5
yanina9045 <yanina.9045@gmail.com>
2026-06-10 01:49:46 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const boton = document.getElementById("btnMensaje");
+const mensaje = document.getElementById("mensaje");
+
+boton.addEventListener("click", function () {
+  mensaje.textContent = "Gracias por visitar mi página personal. Mi compromiso es seguir aprendiendo y ejercer el Derecho con ética, responsabilidad y excelencia.";
+});
